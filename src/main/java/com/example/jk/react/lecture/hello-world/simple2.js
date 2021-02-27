68a51a683e7494d7132a15d6d3b5dcf32f3f9a42
JUNGQUI <ljk2518@gmail.com>
2021-02-27 16:37:50 +0900

    simple2.js

--- src/main/java/com/example/jk/react/lecture/hello-world/simple2.js
+++ src/main/java/com/example/jk/react/lecture/hello-world/simple2.js
@@ -12,5 +12,24 @@ function LikeButton() {
 function Container() {
   const [count, setCount] = React.useState(0);
 
-
+  return React.createElement(
+      'div'
+      , null
+      , React.createElement(LikeButton)
+      , React.createElement(
+          'div'
+          , {style : {marginTop : 20}}
+          , React.createElement('span', null, '현재 카운트 : ')
+          , React.createElement('span', {style : {marginTop : 10}}, count)
+          , React.createElement('button', { onClick : () => setCount(count + 1) }, '증가')
+          , React.createElement('button', { onClick : () => setCount(count - 1) }, '감소')
+      )
+  );
 }
+
+const domContainer = document.getElementById('root');
+
+ReactDOM.render(
+    React.createElement(Container)
+    , domContainer
+);
